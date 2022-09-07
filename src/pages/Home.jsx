4ea758f957da9021914b85c2d8505b40e6023cf0
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-09-07 11:22:53 +0530

    completed home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { SearchBar } from '../components/Searchbar/SearchBar'
 import { Trending } from '../components/Trending/Trending'
 
 export const Home = () => {
+  const [isShowTrending, setisShowTrending] = useState(false);
+  const setisShowTrendingFunction = () => {
+    setisShowTrending(true);
+  }
+  useEffect(()=>{
+    document.body.style.background="url('https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80') no-repeat center";
+    document.body.style.backgroundSize='cover';
+
+  },[])
   return (
-    <div>
-        <SearchBar/>
-        <Trending/>
-    </div>
+    <>
+        <SearchBar setisShowTrending={setisShowTrendingFunction} />
+        {isShowTrending && <Trending />}
+    </>
   )
 }
