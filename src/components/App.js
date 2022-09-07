cbcef4f40fe4229f9fd17de621a2f0a51d4a4448
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-09-07 11:18:56 +0530

    added onFocus funcitonality to searchbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Trending } from './Trending/Trending'
 import { SearchBar } from './Searchbar/SearchBar';
+import { Home } from '../pages/Home';
+
+import { Routes, Route } from 'react-router-dom';
 
 export default function () {
     return (
-        <div>
-            <>
-                <SearchBar />
-                <Trending />
-            </>
-        </div>
+        <Routes>
+            <Route path='/' element={<Home />} />
+        </Routes>
     )
 }
--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -2,11 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import '../Searchbar/searchbar.css'
 import React from 'react'
-export const SearchBar = () => {
+export const SearchBar = ({setisShowTrending}) => {
+
+  const handleSeachFocus=()=>{
+    setisShowTrending();
+  }
+
   return (
     <div className="container">
       <div className='search'>
-        <input className='search-input' placeholder='Search...' type="text" />
+        <input className='search-input' onFocus={handleSeachFocus} placeholder='Search...' type="text" />
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </div>
     </div>
